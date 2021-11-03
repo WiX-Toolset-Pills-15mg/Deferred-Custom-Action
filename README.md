82c362832fc97df591c3f0308dddb58f9c290733
Alexandru Iuga <alex.iuga@gmail.com>
2021-11-03 20:37:20 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Deferred Custom Action
+
+## Question
+
+- How to create a deferred custom action?
+
+## Description
+
+This pill demonstrates how to create a deferred custom action.
+
+A deferred custom action when it is encountered in the install sequence it is jut put in a list and it is executed at the end of the install sequence.
+
---
+++ sources/DeferredCustomAction.CustomActions/LogSomethingCustomAction.cs
@@ -0,0 +1,40 @@
+// Wix Toolset Pills 15mg
+// Copyright (C) 2019 - 2021 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace DustInTheWind.DeferredCustomAction.CustomActions
+{
+    public class LogSomethingCustomAction
+    {
+        [CustomAction("LogSomething")]
+        public static ActionResult Execute(Session session)
+        {
+            try
+            {
+                session.Log("Begin LogSomething custom action");
+
+                session.Log("This is a demo, to show how to create and execute a deferred custom action.");
+
+                return ActionResult.Success;
+            }
+            finally
+            {
+                session.Log("End LogSomething custom action");
+            }
+        }
+    }
+}
